37b5f404667d6a1095d107e99b9c6cceb73a1984
Adrian Sarno <mas3704@g.harvard.edu>
2025-10-04 19:10:23 -0700

    add hotfix for parquet

--- model_container/main.py
+++ model_container/main.py
@@ -1,11 +1,13 @@
+
+
 def main():
     import pandas as pd
 
     bucket_name = "dosewisedb"
-    file_path_in_bucket = "hemodyn_table.csv"
+    file_path_in_bucket = "hemodyn_table"
 
     # Directly read from GCS
-    df = pd.read_csv(f"gs://{bucket_name}/{file_path_in_bucket}")
+    df = pd.read_parquet(f"gs://{bucket_name}/{file_path_in_bucket}")
 
     print(df.head())
 
